4c1786e500cc9b3e1f0e56bc3a4f96c495c8691b
Sasha <73294558+2JustFox2@users.noreply.github.com>
2026-02-17 17:19:57 +0300

    Add Decimal.js dependency and implement AppState class

--- lab-1/src/App.tsx
+++ lab-1/src/App.tsx
@@ -1,7 +1,25 @@
 import './App.css';
 import { Panel, PrintDocument, TextBlock, TreeView } from './shared';
+import Decimal from 'decimal.js';
 
+class AppState {
+    double: number = 12.34;
+    readonly decimal: Decimal = new Decimal(12.34);
+    uint: Uint16Array = new Uint16Array(1);
+
+    Enum = {
+        First: 1,
+        Second: 2,
+        Third: 3
+    };
+
+    constructor() {
+        this.uint[0] = 12345;
+    }
+}
 function App() {
+    const app = new AppState();
+    console.log(app)
     return (
         <>
             <div className="main">
